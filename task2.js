61961ac258ba8a8d5d440e686966319bf49c6395
Lilit Mkrtchyan <lil.mkrtchyan.02@gmail.com>
2021-10-01 23:34:31 +0400

    feat.(task2.js): Strings

---
+++ task2.js
@@ -0,0 +1,7 @@
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // embed string "1" in  string "Hello "
+
+console.log( `hello ${"name"}` ); // will output "Hello "name" "
+
+console.log( `hello ${name}` ); // will output "Hello Ilya"
